90b56f9322672bf6f0553f499fc886d078b3da79
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-09 13:43:28 -0500

    changes event to party (to allow me to read code easier to find errors

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // === Constants ===
 const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
-const COHORT = "2604-FTB-ET-WEB-FT";
+const COHORT = "/2604-JAMES";
 const API = BASE + COHORT;
 
 // === State ===
-let events = [];
-let selectedEvent;
+let parties = [];
+let selectedParty;
 let rsvps = [];
 let guests = [];
 
 /**Update state with all events from the API
  */
-async function getEvents() {
+async function getParties() {
   try {
     const response = await fetch(API + "/events");
     const result = await response.json();
-    events = result.data;
+    parties = result.data;
     render();
   } catch (error) {
     console.error(error);
@@ -24,11 +24,11 @@ async function getEvents() {
 
 /**Update state with single event from API
  */
-async function getEvent(id) {
+async function getParty(id) {
   try {
-    const response = await fetch(API + "/events" + id);
+    const response = await fetch(API + "/events/" + id);
     const result = await response.json();
-    selectedEvent = result.data;
+    selectedParty = result.data;
     render();
   } catch (error) {
     console.error(error);
@@ -62,61 +62,73 @@ async function getGuests() {
 // === Components ===
 
 /**I need a single list item containing details about the event when clicked */
-function EventListItem() {
+function PartyListItem(party) {
   const $li = document.createElement("li");
 
-  if (event.id === selectedEvent.id) {
+  if (party.id === selectedParty?.id) {
     $li.classList.add("selected");
   }
 
-  $li.innerHTML = `<a href="#selected">${event.name}</a>
+  $li.innerHTML = `
+    <a href="#selected">${party.name}</a>
   `;
-  $li.addEventListener("click", () => getEvent(event.id));
+  $li.addEventListener("click", () => getParty(party.id));
+
   return $li;
 }
 
 /**I need a list of event names */
-function EventList() {
+function PartyList() {
   const $ul = document.createElement("ul");
-  $ul.classList.add("events");
+  $ul.classList.add("parties");
 
-  const $events = events.map(EventListItem);
-  $ul.replaceChildren(...$events);
+  const $parties = parties.map(PartyListItem);
+  $ul.replaceChildren(...$parties);
 
   return $ul;
 }
 
 /** I need a function to show selected event information */
-function SelectedEvent() {
-  if (!selectEvent) {
+function SelectedParty() {
+  if (!selectedParty) {
     const $p = document.createElement("p");
-    $p.textContent = "Please select a event to learn more.";
+    $p.textContent = "Please select a party to learn more.";
+
     return $p;
   }
 
-  const $event = document.createElement("section");
-  $event.innerHTML = `
-    <h3>${selectedEvent.name} #${selectedEvent.id}</h3>
-    <time datetime="${selectedEvent.date}">
-      ${selectedEvent.date.slice(0, 10)}
+  const $party = document.createElement("section");
+  $party.innerHTML = `
+    <h3>${selectedParty.name} #${selectedParty.id}</h3>
+    <time datetime="${selectedParty.date}">
+      ${selectedParty.date.slice(0, 10)}
     </time>
-    <address>${selectedEvent.location}<?address>
-    <p>&{selectedEvent.description}</p>
+    <address>${selectedParty.location}</address>
+    <p>${selectedParty.description}</p>
     <GuestList></GuestList>
     `;
-  $event.querySelector("GuestList").replaceWith(GuestList());
+  $party.querySelector("GuestList").replaceWith(GuestList());
 
-  return $event;
+  return $party;
 }
 
 /** I need a list of guests for the SelectedEvent */
 function GuestList() {
   const $ul = document.createElement("ul");
-  const selectedGuestList = guests.filter((guest) =>
+  const guestsAtParty = guests.filter((guest) =>
     rsvps.find(
-      (rsvp) => rsvp.guestId === guest.id && rsvp.eventId === selectedEvent.id,
+      (rsvp) => rsvp.guestId === guest.id && rsvp.eventId === selectedParty.id,
     ),
   );
+
+  const $guests = guestsAtParty.map((guest) => {
+    const $guest = document.createElement("li");
+    $guest.textContent = guest.name;
+    return $guest;
+  });
+  $ul.replaceChildren(...$guests);
+
+  return $ul;
 }
 // === Render ===
 
@@ -124,7 +136,7 @@ function render() {
   const $app = document.querySelector("#app");
   //I need similar HTML elements as the gala guided practice, changing h1 to event Planner, 1st h2 to Upcoming Parties, ArtistList to eventList, 2nd h2 to event Details, ArtistDetails to eventDetails
   $app.innerHTML = `
-    <h1>event Planner</h1>
+    <h1>Party Planner</h1>
     <main>
       <section>
         <h2>Upcoming Parties</h2>
@@ -136,11 +148,14 @@ function render() {
       </section>
     </main>
   `;
-  $app.querySelector("PartyList").replaceWith(EventList());
-  $app.querySelector("PartyDetails").replaceWith(EventDetails());
+  $app.querySelector("PartyList").replaceWith(PartyList());
+  $app.querySelector("PartyDetails").replaceWith(SelectedParty());
 }
 
 async function init() {
+  await getParties();
+  await getRsvps();
+  await getGuests();
   render();
 }
 
